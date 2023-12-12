f7997bf3d9dc6b1ea26de730c6af5e66d3d5908d
catura <catura.ba@gmail.com>
2023-12-12 23:40:31 +0900

    setapi

--- api.go
+++ api.go
@@ -1,6 +1,31 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func WriteJSON(w http.ResponseWriter, status int , v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+type apiFunc func (http.ResponseWriter, *http.Request) error
+
+type ApiError struct{
+	Error string
+}
+
+func makeHTTPHandleFunc (f apiFunc) http.HandlerFunc {
+	return func (w http.ResponseWriter, r *http.Request)  {
+		if err := f(w,r); err != nil {
+			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+		}
+	}
+}
 
 type APIServer struct {
 	listenAddr string
@@ -13,25 +38,27 @@ func NewAPIServer(listenAddr string) *APIServer {
 }
 
 func (s *APIServer) Run(){
-	
+	router := mux.NewRouter()
+
+	router.HandleFunc("/account", makeHTTPHandleFunc(s.handlerAccount))
 }
 
-func (s *APIServer) handlerAccount(w *http.ResponseWriter, r *http.Request) error {
+func (s *APIServer) handlerAccount(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
-func (s *APIServer) handlerGetAccount(w *http.ResponseWriter, r *http.Request) error {
+func (s *APIServer) handlerGetAccount(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
-func (s *APIServer) handlerCreateAccount(w *http.ResponseWriter, r *http.Request) error {
+func (s *APIServer) handlerCreateAccount(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
-func (s *APIServer) handlerDeleteAccount(w *http.ResponseWriter, r *http.Request) error {
+func (s *APIServer) handlerDeleteAccount(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
-func (s *APIServer) handlerTransfer(w *http.ResponseWriter, r *http.Request) error {
+func (s *APIServer) handlerTransfer(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
