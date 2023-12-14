8f5202bf0540316ca82d04bc2e7602fda3e53c83
catura <catura.ba@gmail.com>
2023-12-14 20:22:13 +0900

    setapi

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Yeah!!")
+	LoadEnv()
+	fmt.Println(getPort())
+	server := NewAPIServer(":3000")
+	server.Run()
+}
+
+func getPort() string{
+	port := os.Getenv("DB_PORT")
+	if port == ""{
+		return ":3000"
+	}
+
+	return ":" + port
+}
+
+func LoadEnv(){
+	if err := godotenv.Load(); err != nil{
+		log.Fatal("Error load .env file")
+	}
 }
